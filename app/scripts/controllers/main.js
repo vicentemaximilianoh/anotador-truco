400c655fb76ae6fcd380bfb46b18ac46427c2f11
Maximiliano Vicente <vicente.maximilianoh@gmail.com>
2014-08-26 11:44:21 -0300

    Imagenes + estetica + borrado de puntos

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -17,21 +17,32 @@ angular.module('anotadorTrucoApp')
     ];
     */
     $scope.point = 1;
+    $scope.pointSub = -1;
 
     //Us and they score
     $scope.us = 0;
+    $scope.usTeam = 'us';
     $scope.they = 0;
+    $scope.theyTeam = 'they';
 
-    $scope.addScore = function(data, event, team){
-    	var score;
-    	if(team === 'us'){
-    		$scope.us++;
-    		score = $scope.us;
-    	}else if(team === 'they'){
-    		$scope.they++;
-    		score = $scope.they;
+    $scope.modifyScore = function(data, event, team){
+    	console.log(data, event);
+    	if(!isNaN(data)){
+	    	var score;
+	    	if(team === 'us'){
+	    		$scope.us+=data;
+	    		score = $scope.us;
+	    	}else if(team === 'they'){
+	    		$scope.they+=data;
+	    		score = $scope.they;
+	    	}
+
+	    	$scope.drawBoxes(team, score);
     	}
 
+    };
+
+    $scope.drawBoxes = function(team, score){
     	var team_id = "#score-" + team;
     	$(team_id).html("");
 
@@ -40,14 +51,34 @@ angular.module('anotadorTrucoApp')
     		for(var i=1;i<=five;i++){
     			var five_box = '<div class="score score-5"></div>'; 
     			$(team_id).append(five_box);
+    			if(i === 3){
+    				$(team_id).append('<hr/>');
+    			}
     		}
     	}
 
     	var modules = score % 5;
     	if(modules != 0){
+    		console.log(five, modules);
+    		if(five === 3){
+    			$(team_id).append('<hr/>');
+
+    		}
     		var box = '<div class="score score-'+modules+'"></div>'; 
     		$(team_id).append(box);
     	}
     };
+
+    $scope.substractScore = function(data, event){
+    	var score;
+    	if(data === 'they'){
+    		$scope.they++;
+    		score = $scope.they;
+    	}else if(data === 'us'){
+    		$scope.us++;
+			score = $scope.us;
+    	}
+    	console.log(data);
+    }
 
   });
